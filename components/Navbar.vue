b7777b89c3732f38f4edf06cffea60261de57642
Nob Yuasys <yuasys@gmail.com>
2023-08-30 23:14:38 +0900

    new file:   components/Navbar.vue

---
+++ components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+              <NuxtLink class="navbar-brand" to="/">YuasysChannel</NuxtLink>
+             
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div class="navbar-nav" >
+                      <NuxtLink class="nav-link" activeClass="active" aria-current="page" to="/">Home</NuxtLink>
+                      <NuxtLink class="nav-link" activeClass="active" to="/secondpage">Secondpage</NuxtLink>
+                  </div>
+              </div>
+          </div>
+      </nav>
+  </div>
+</template>
