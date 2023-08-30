755a07c7803b4ccb5d992bae76cd3cec38bda08b
Nob Yuasys <yuasys@gmail.com>
2023-08-30 22:35:13 +0900

    modified:   nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    ssr: true,
+    app:{
+        head: {
+          title: "My Blog",
+          meta: [
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+          ],
+          link: [
+            { rel: "icon",type: "image/x-icon", href: "/favicon.ico" }, 
+            {rel: "stylesheet",href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"}
+          ],
+        },
+      }
+
 })
