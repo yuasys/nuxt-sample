5aea6fdd7ed16caf6d3ec37a50f2cf5fa512d187
Nob Yuasys <yuasys@gmail.com>
2023-08-31 00:43:36 +0900

    new file:   pages/[...slug].vue

---
+++ pages/[...slug].vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container-fluid">
+    <h1>Not Found</h1>
+    <NuxtLink to="/">Go Back</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+  setResponseStatus();
+</script>
