c5676d66134782b8dc1210264a8b4b46cba27e00
Nob Yuasys <yuasys@gmail.com>
2023-08-31 20:55:06 +0900

    new file:   pages/fishdata/index.vue

---
+++ pages/fishdata/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="pt-3">
+    <h1 class="text-2xl pb-6 text-red-700">Fish data</h1>
+    <nuxt-link class="underline underline-offset-2" to="/">Goto Home</nuxt-link>
+  </div>
+</template>
