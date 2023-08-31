7fd7c32599e300d456a2b1ca24f9ec6473b714dc
Nob Yuasys <yuasys@gmail.com>
2023-08-31 14:47:46 +0900

    modified:   pages/secondpage.vue

--- pages/secondpage.vue
+++ pages/secondpage.vue
@@ -1,6 +1,13 @@
+<script>
+// This will work in both `<script setup>` and `<script>`
+definePageMeta({
+  layout: "custom",
+});
+</script>
 <template>
   <div class="pt-3">
     <h1 class="text-2xl pb-6 text-red-700">SecondPageですよ</h1>
     <NuxtLink class="underline underline-offset-2" to="/">Goto Home</NuxtLink>
+    <p class="text-red-600 py-3">※このページは独自にカスタマイズしたレイアウトを使っています。</p>
   </div>
 </template>
