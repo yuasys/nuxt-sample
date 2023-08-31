932ffdf9837446bf18ce75d772defa30dc8986af
Nob Yuasys <yuasys@gmail.com>
2023-08-31 10:19:31 +0900

    modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
-    "./pages/**.*.vue",
+    "./pages/**/*.vue",
     "./plugins/**.*{js,ts}",
     "./nuxt.config.{js,ts}",
     "./app.vue",
