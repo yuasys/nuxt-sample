4100c0a1d6f87ccb2980bf4c01c5554eaddf0ad9
Nob Yuasys <yuasys@gmail.com>
2023-08-31 10:20:34 +0900

    modified:   pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-pink-50 p-6">
-    <h1 class="text-3xl pb-2 text-slate-700">Index</h1>
-    <NuxtLink to = "/secondpage">SecondPage</NuxtLink>
+  <div class="pt-3">
+    <h1 class="text-2xl pb-6 text-red-700">Indexのページです</h1>
+    <NuxtLink class="underline underline-offset-2" to = "/secondpage"><span >Goto SecondPage</span></NuxtLink>
   </div>
 </template>
