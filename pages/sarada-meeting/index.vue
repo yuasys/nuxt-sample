63621dddd505825da3b41ee7a34e0fa88e2a31ea
Nob Yuasys <yuasys@gmail.com>
2023-09-03 12:25:50 +0900

    new file:   pages/sarada-meeting/index.vue

---
+++ pages/sarada-meeting/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+definePageMeta({
+  layout: "sarada",
+});// This will work in both `<script setup>` and `<script>`
+</script>
+
+<template>
+  <p>templateの中です</p>
+</template>
+
+<style>
+
+</style>
