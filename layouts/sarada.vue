4099526e7c620c366b1dfeff2954f19b2e1d9995
Nob Yuasys <yuasys@gmail.com>
2023-09-04 05:03:48 +0900

    modified:   layouts/sarada.vue

--- layouts/sarada.vue
+++ layouts/sarada.vue
@@ -5,11 +5,24 @@
 </template>
 
 <script setup lang="ts">
-// ヘッダーメタ情報を設定
+// ヘッダーメタ情報を設定 【useHeadを使った場合】
   useHead({
     title: 'サラダ・ミーティング',
     meta: [
       { name: 'description', content: '新鮮なつながりを、ひとつのサラダから'}
     ]
   })
+
+// ヘッダーメタ情報を設定【useSeoMataを使った場合】
+/*
+useSeoMeta{{
+  title: 'サラダ・ミーティング２',
+  ogTitle: 'サラダ・ミーティング２',
+  Description: '新鮮なつながりを、ひとつのサラダから',
+  ogDescription: '新鮮なつながりを、ひとつのサラダから',
+  ogImage: '',
+  twitterCard: '',
+}}
+
+*/
 </script>
