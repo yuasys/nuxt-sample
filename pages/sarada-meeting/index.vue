2efd844e11745db5d6c0cdc0de5218340ef83da0
Nob Yuasys <yuasys@gmail.com>
2023-09-03 16:48:20 +0900

    modified:   pages/sarada-meeting/index.vue

--- pages/sarada-meeting/index.vue
+++ pages/sarada-meeting/index.vue
@@ -8,6 +8,31 @@
   <div>
     <img src="/sarada/image01.jpg" alt="sarada"/>
   </div>
+<!-- 
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>タイトル</title>
+-->
+  <!-- リセットCSS -->
+  <!-- 
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@3.0.2/destyle.css">
+  -->
+  <!-- style.css -->
+  <!--
+<link rel="stylesheet" href="./css/style.css">
+  -->
+  <!-- Google font -->
+<!-- 
+</head>
+<body>
+  <header></header>
+  <main></main>
+  <footer></footer>
+</body>
+</html>   -->
 </template>
 
 <style>
