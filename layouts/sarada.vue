ab3bbe45c216dd8d3a210b2f869cc67f3528bc04
Nob Yuasys <yuasys@gmail.com>
2023-09-03 16:48:05 +0900

    modified:   layouts/sarada.vue

--- layouts/sarada.vue
+++ layouts/sarada.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="bg-slate-50">
+  <div class="bg-slate-50 p-12">
     <slot />
   </div>
 </template>
+
+<script setup lang="ts">
+// ヘッダーメタ情報を設定
+  useHead({
+    title: 'サラダ・ミーティング',
+    meta: [
+      { name: 'description', content: '新鮮なつながりを、ひとつのサラダから'}
+    ]
+  })
+</script>
